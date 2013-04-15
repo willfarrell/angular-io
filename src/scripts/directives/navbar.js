8a7e0db41c13985e8ee8f555eadeb923d44afd59
will Farrell <will.farrell@gmail.com>
2013-04-14 21:45:14 -0400

    Added AngularStrap navbar (w/ no jQuery dependency)

---
+++ src/scripts/directives/navbar.js
@@ -0,0 +1,39 @@
+/*
+From AngularStrap, modified to not require jQuery
+
+Remove when commited to AngularStrap
+
+*/
+
+angular.module('$strap.directives')
+
+.directive('bsNavbar', ['$location', function($location) {
+  'use strict';
+
+  return {
+    restrict: 'A',
+    link: function postLink(scope, element, attrs, controller) {
+      // Watch for the $location
+      scope.$watch(function() {
+        return $location.path();
+      }, function(newValue, oldValue) {
+
+        var li = element[0].querySelectorAll('[data-match-route]'),
+          i = li.length;
+        
+        while (i--) {
+          var $li = angular.element(li[i]),
+            // data('match-rout') does not work with dynamic attributes
+            pattern = $li.attr('data-match-route'),
+            regexp = new RegExp('^' + pattern + '$', ["i"]);
+
+          if(regexp.test(newValue)) {
+            $li.addClass('active');
+          } else {
+            $li.removeClass('active');
+          }
+        }
+      });
+    }
+  };
+}]);
