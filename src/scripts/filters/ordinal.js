bb684d7aaf94896116ef6e5f3efbce98f77ab7e6
will Farrell <will.farrell@gmail.com>
2013-04-19 01:42:57 -0400

    require and todo comments

--- src/scripts/filters/ordinal.js
+++ src/scripts/filters/ordinal.js
@@ -1,8 +1,16 @@
+/*
+Requires:
+- lang: en
+
+TODO
+- use locale string appends
+*/
+
 angular.module('io.filters')
 .filter('ordinal', function() {
 	return function(num) {
-		var s=['th','st','nd','rd'],
-			v=num%100;
-		return num+(s[(v-20)%10]||s[v]||s[0]);
+		var append=['th','st','nd','rd'],
+			value=num%100;
+		return num+(append[(value-20)%10]||append[value]||append[0]);
 	};
 });
